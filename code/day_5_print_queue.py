8514b45964b1c08a9a17fcb249b9f8de3a06b10b
Leelakrishna Kantamneni <leelakrishnak@meta.com>
2025-01-31 12:41:40 -0800

    day5 sum added comments

--- code/day_5_print_queue.py
+++ code/day_5_print_queue.py
@@ -1,10 +1,12 @@
 ###
 # Problem : https://adventofcode.com/2024/day/5
+# Both Part1 and Part2
 #
 from functools import cmp_to_key
 
 rules = {}
 
+# Process input
 def main():
     with open("input_text.txt", "r") as f:
         lines = f.readlines()
@@ -16,21 +18,22 @@ def main():
             rules.setdefault(values[0], []).append(('<', values[1]))
             rules.setdefault(values[1], []).append(('>', values[0]))
 
-    result = 0
-    corrected_sum = 0
+    correct_order_seq_sum = 0
+    incorrect_order_seq_sum =  0
     for line in lines:
         line = line.rstrip('\n')
         if "," in line:
             values = line.split(",")
             currMap = {y: x for x, y in enumerate(values)}
             if not verify_rules(values, rules, currMap):
-                corrected_sum += int(find_middle(sort_seq(values)))
+                incorrect_order_seq_sum += int(find_middle(sort_seq(values)))
                 continue
             else:
-                result += int(find_middle(values))
-    print(result)
-    print(corrected_sum)
+                correct_order_seq_sum += int(find_middle(values))
+    print(correct_order_seq_sum)
+    print(incorrect_order_seq_sum)
 
+# Custom comparison for incorrect sequence
 def compare_pages(x, y):
     for (op, val) in rules[x]:
         if y != val:
@@ -41,17 +44,20 @@ def compare_pages(x, y):
             return 1
     return 0
     
+# Sort function
 def sort_seq(lst)->list:
     lst.sort(key=cmp_to_key(compare_pages))
     return lst
 
+# Find middle value
 def find_middle(lst)->int:
     middle_index = len(lst) // 2
     if len(lst) % 2 == 0:
         return (lst[middle_index - 1], lst[middle_index])
     else:
         return lst[middle_index]
-    
+
+# Verify if the value is in correct position in the sequence or not
 def verify_rules(values, rules, currMap):
     for i, v in enumerate(values):
         if v in rules:
